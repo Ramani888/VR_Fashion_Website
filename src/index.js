97274c94598c054102ca6300cd478c986888450b
Divyeshrzeusint <divyeshr@zeusint.com>
2024-09-18 07:47:18 +0530

    webite setup done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// // Css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'animate.css/animate.css';
+import 'magnific-popup/dist/magnific-popup.css';
+import './assets/css/font-awesome.min.css';
+import './assets/css/flaticon.css';
+import './assets/fonts/flaticon/flaticon-2.css';
+import './assets/css/default.css';
+import './assets/css/style.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
